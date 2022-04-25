754b7cd7150ddcfaec436661dd2fb840cd10e959
jurody <kamilov.2000@gmail.com>
2022-04-25 17:04:39 +0500

    hehe

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementsByTagName("body")[0].style.opacity = opacity / 100;
     if (opacity <= 0) {
       document.getElementsByTagName("body")[0].style.display = "none";
+      var body = document.createElement("body");
+      body.innerHTML =
+        "<h1 class='text-center'>If you think you can get site without kissing my cock, he-he. You are dead wrong!</h1>";
     }
   }, 1000);
 });
