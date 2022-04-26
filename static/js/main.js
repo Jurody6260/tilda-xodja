755f3eb9e68aec43ac55e55e0f4b9ca138fe3139
jurody <kamilov.2000@gmail.com>
2022-04-26 12:16:59 +0500

    interval clear

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,13 @@
 var opacity = 100;
 document.addEventListener("DOMContentLoaded", function () {
-  setInterval(function () {
+  var refreshIntervalId = setInterval(function () {
     if (opacity > 0) opacity -= 5;
     document.getElementsByTagName("body")[0].style.opacity = opacity / 100;
     if (opacity <= 0) {
       document.getElementsByTagName("body")[0].style.opacity = 100;
       document.getElementsByTagName("body")[0].innerHTML =
         "<h1 class='text-center'>If you think you can get site without kissing my cock, he-he. You are dead wrong!</h1>";
+      clearInterval(refreshIntervalId);
     }
   }, 1000);
 });
